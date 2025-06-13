efd480d05543be843c2122c6538613da81b44afc
shan <peanshan@gmail.com>
2025-06-14 04:02:27 +0800

    chore: adjust api rewrites settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://youtube-mp3-converter.up.railway.app/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
