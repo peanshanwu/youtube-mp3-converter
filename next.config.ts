f1b494f4ad61a66e17366890bdcf8ee0227545be
shan <peanshan@gmail.com>
2025-06-16 22:18:14 +0800

    feat: adjust next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['i.ytimg.com'],
+  },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: 'https://youtube-mp3-converter.up.railway.app/:path*',
+        // destination: 'http://localhost:8000/:path*',
       },
     ]
   },
